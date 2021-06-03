2e89cb15b70c39a273c79932740b124976afd928
JoelM00 <61049184+JoelM00@users.noreply.github.com>
2021-06-03 21:37:39 +0100

    update

---
+++ Projeto/Projeto/IDataAccess.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Projeto
+{
+    public interface IDataAccess
+    {
+        Task<List<T>> loadData<T, U>(string sql, U parameters, string connectionString);
+        Task saveData<T>(string sql, T parameters, string connectionString);
+    }
+}
