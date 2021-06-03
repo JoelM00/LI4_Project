1d6555b03054616c7e9020b81b24d58c1047065b
JoelM00 <61049184+JoelM00@users.noreply.github.com>
2021-06-03 21:53:15 +0100

    ligacao

--- Projeto/Projeto/obj/Debug/net5.0/RazorDeclaration/Pages/People.razor.g.cs
+++ Projeto/Projeto/obj/Debug/net5.0/RazorDeclaration/Pages/People.razor.g.cs
@@ -82,6 +82,20 @@ using Projeto;
 #line default
 #line hidden
 #nullable disable
+#nullable restore
+#line 4 "C:\Users\Joel\Documents\GitHub\LI4\Projeto\Projeto\Pages\People.razor"
+using Projeto.Models;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 5 "C:\Users\Joel\Documents\GitHub\LI4\Projeto\Projeto\Pages\People.razor"
+using Microsoft.Extensions.Configuration;
+
+#line default
+#line hidden
+#nullable disable
     [Microsoft.AspNetCore.Components.RouteAttribute("/people")]
     public partial class People : Microsoft.AspNetCore.Components.ComponentBase
     {
@@ -91,20 +105,22 @@ using Projeto;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 10 "C:\Users\Joel\Documents\GitHub\LI4\Projeto\Projeto\Pages\People.razor"
+#line 25 "C:\Users\Joel\Documents\GitHub\LI4\Projeto\Projeto\Pages\People.razor"
              
+    List<PersonModel> people;
 
-        protected override async Task OnInitializedAsync()
-        {
-            //await _data.loadData
-        }
+    protected override async Task OnInitializedAsync()
+    {
+        string sql = "select * from people";
 
-
+        people = await _data.loadData<PersonModel, dynamic>(sql, new { }, _config.GetConnectionString("default"));
+    }
 
 #line default
 #line hidden
 #nullable disable
-        [global::Microsoft.AspNetCore.Components.InjectAttribute] private IDataAccess _dada { get; set; }
+        [global::Microsoft.AspNetCore.Components.InjectAttribute] private IConfiguration _config { get; set; }
+        [global::Microsoft.AspNetCore.Components.InjectAttribute] private IDataAccess _data { get; set; }
     }
 }
 #pragma warning restore 1591
